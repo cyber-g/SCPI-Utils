cf2b00321814fd66337a990d9d90f4d20346234a
Cyber-G <cygerpham@free.fr>
2025-11-28 17:53:25 +0100

    get_screenshot.py: refactor using scpi_helpers module

--- get_screenshot.py
+++ get_screenshot.py
@@ -23,10 +23,11 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 import socket
 from datetime import datetime
 import argparse
+import scpi_helpers as scpih
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description="Capture screenshot from instrument")
-parser.add_argument('-i', '--ip', default='192.168.145', help='IP address of the instrument')
+parser.add_argument('-i', '--ip', default='192.168.1.45', help='IP address of the instrument')
 parser.add_argument('-p', '--port', type=int, default=5025, help='Port number')
 parser.add_argument('-r', '--remote_filename', default=None, help='Remote file name')
 parser.add_argument('-l', '--local_filename', default=None, help='Local file name')
@@ -47,26 +48,14 @@ FF_IP = args.ip
 PORT = args.port
 
 # Connect to FieldFox
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((FF_IP, PORT))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((FF_IP, PORT))
 
 # Capture screen on FieldFox
-s.sendall(b'MMEM:STOR:IMAG "' + args.remote_filename.encode() + b'"\n')
+scpih.send(sock, f'MMEM:STOR:IMAG "{args.remote_filename}"')
 
 # Request the file via BINBLOCK
-s.sendall(b'MMEM:DATA? "' + args.remote_filename.encode() + b'"\n')
-
-# Read BINBLOCK header '#<digits><length>'
-header = s.recv(2)         # first two characters: # and digit count
-assert header[0:1] == b'#'
-num_digits = int(header[1:2].decode())
-
-length = int(s.recv(num_digits).decode())
-
-# Read the PNG binary data
-data = b""
-while len(data) < length:
-    data += s.recv(length - len(data))
+data = scpih.query_binblock(sock, f'MMEM:DATA? "{args.remote_filename}"')
 
 # Save to Linux filesystem
 with open(args.local_filename, "wb") as f:
@@ -74,4 +63,4 @@ with open(args.local_filename, "wb") as f:
 
 print(f"Saved {args.local_filename} ({len(data)} bytes)")
 
-s.close()
+sock.close()
