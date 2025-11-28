df0ba6cd16bff7288453e3401298cea3618a92b4
Cyber-G <cygerpham@free.fr>
2025-11-28 18:01:28 +0100

    get_hcopy.py: write header

--- get_hcopy.py
+++ get_hcopy.py
@@ -1,8 +1,24 @@
-# HCOPy:IMAGe:FORMat PNG
-# HCOPy:REGion FULL
-# HCOPy:EXECute
-# HCOPy:DATA?
+#!/usr/bin/env python3
+"""
+SCPI Utils - HCopy Screenshot Capture (HCOPy:EXEC)
 
+Project: https://github.com/cyber-g/SCPI-Utils
+
+Copyright (C) 2025 Germain PHAM <cygerpham@free.fr>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
 
 import socket
 from datetime import datetime
