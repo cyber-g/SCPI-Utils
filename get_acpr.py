755cb81922ecdf1c95eac38e4e861cb0a790aa5d
Cyber-G <cygerpham@free.fr>
2026-01-30 20:39:08 +0100

    get_acpr.py: add command line parameters

--- get_acpr.py
+++ get_acpr.py
@@ -2,39 +2,48 @@
 import socket
 import scpi_helpers as scpih
 import time
+import argparse
 
-FIELDFOX_IP   = "192.168.1.45"   
-FIELDFOX_PORT = 5025
+# Defaults (can be overridden via CLI)
+INSTRUMENT_IP   = "192.168.1.45"
+INSTRUMENT_PORT = 5025
 
 CENTER_FREQ_HZ = 2.39995e9
 MAIN_BW_HZ     = 1e6
 ADJ_OFFSET_HZ  = 1e6
 ADJ_BW_HZ      = 1e6
 
-
-
+def parse_args():
+    p = argparse.ArgumentParser(description="Measure ACPR on FieldFox via SCPI")
+    p.add_argument("-i", "--ip",        dest="ip",          default=INSTRUMENT_IP, help="Instrument IP or hostname")
+    p.add_argument("-p", "--port",      dest="port",        type=int, default=INSTRUMENT_PORT, help="Instrument port (TCP)")
+    p.add_argument("-c", "--center-freq", dest="center_freq", type=float, default=CENTER_FREQ_HZ, help="Center frequency in Hz")
+    p.add_argument("-m", "--main-bw",     dest="main_bw",     type=float, default=MAIN_BW_HZ, help="Main channel bandwidth in Hz")
+    p.add_argument("-o", "--adj-offset",  dest="adj_offset",  type=float, default=ADJ_OFFSET_HZ, help="Adjacent channel offset in Hz")
+    p.add_argument("-b", "--adj-bw",      dest="adj_bw",      type=float, default=ADJ_BW_HZ, help="Adjacent channel bandwidth in Hz")
+    return p.parse_args()
 
 # ------------------
 # Setup ACPR
 # ------------------
 
-def setup_acpr(sock):
+def setup_acpr(sock, center_freq=CENTER_FREQ_HZ, main_bw=MAIN_BW_HZ, adj_offset=ADJ_OFFSET_HZ, adj_bw=ADJ_BW_HZ):
     # Select SA mode
     scpih.send(sock, 'INST:SEL "SA"')
     scpih.query_singleline(sock, "*OPC?")  # wait until mode switch completed
 
     # Center frequency
-    scpih.send(sock, f"SENS:FREQ:CENT {CENTER_FREQ_HZ}")
+    scpih.send(sock, f"SENS:FREQ:CENT {center_freq}")
 
     # Select ACPR as the channel measurement
     scpih.send(sock, "SENS:MEAS:CHAN ACPR")
 
     # Main channel integration bandwidth
-    scpih.send(sock, f"SENS:CME:IBW {MAIN_BW_HZ}")
+    scpih.send(sock, f"SENS:CME:IBW {main_bw}")
 
-    # Adjacent channels: ± 1 MHz at 1 MHz BW
-    scpih.send(sock, f"SENS:ACPower:OFFS1:BWID {ADJ_BW_HZ}")
-    scpih.send(sock, f"SENS:ACPower:OFFS1:FREQ {ADJ_OFFSET_HZ}")
+    # Adjacent channels: ± offset at adj bandwidth
+    scpih.send(sock, f"SENS:ACPower:OFFS1:BWID {adj_bw}")
+    scpih.send(sock, f"SENS:ACPower:OFFS1:FREQ {adj_offset}")
     scpih.send(sock, "SENS:ACPower:OFFS1:STAT ON")
 
     # Turn off other offsets
@@ -86,9 +95,11 @@ def measure_acpr(sock):
 # ------------------
 
 def main():
+    args = parse_args()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(5.0)
-    sock.connect((FIELDFOX_IP, FIELDFOX_PORT))
+    sock.connect((args.ip, args.port))
 
     try:
         print("Connected to:", scpih.query_singleline(sock, "*IDN?"))
@@ -99,8 +110,8 @@ def main():
         # pause for 1 seconds to allow autoscale to complete
         time.sleep(1)
 
-        # Setup ACPR measurement
-        setup_acpr(sock)
+        # Setup ACPR measurement with CLI parameters
+        setup_acpr(sock, center_freq=args.center_freq, main_bw=args.main_bw, adj_offset=args.adj_offset, adj_bw=args.adj_bw)
 
         # Trigger Measure ACPR
         acpr = measure_acpr(sock)
